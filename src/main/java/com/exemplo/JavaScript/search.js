c4834ad2f973704f3ee56a9a4f370116215b3b7b
Thiago dos Santos <supremoedits12066@gmail.com>
2023-11-11 07:14:40 -0300

    Script search finalizado

--- src/main/java/com/exemplo/JavaScript/search.js
+++ src/main/java/com/exemplo/JavaScript/search.js
@@ -1,6 +1,37 @@
-/* 
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Other/javascript.js to edit this template
- */
+document.addEventListener('DOMContentLoaded', function () {
+    const inputCadastro = document.getElementById('inputCadastro');
+    const tableRows = document.querySelectorAll('#table-produtos tbody tr');
 
+    inputCadastro.addEventListener('input', function () {
+        const searchText = inputCadastro.value.toLowerCase();
 
+        tableRows.forEach(function (row) {
+            const rowData = row.innerText.toLowerCase();
+
+            if (rowData.includes(searchText)) {
+                row.style.display = '';
+            } else {
+                row.style.display = 'none';
+            }
+        });
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    const inputVendas = document.getElementById('inputVendas');
+    const tableRows = document.querySelectorAll('#table-vendas tbody tr');
+
+    inputVendas.addEventListener('input', function () {
+        const searchText = inputVendas.value.toLowerCase();
+
+        tableRows.forEach(function (row) {
+            const rowData = row.innerText.toLowerCase();
+
+            if (rowData.includes(searchText)) {
+                row.style.display = '';
+            } else {
+                row.style.display = 'none';
+            }
+        });
+    });
+});
