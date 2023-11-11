37d60c29aae427b0606effbc792c0f79f66e0344
Thiago dos Santos <supremoedits12066@gmail.com>
2023-11-11 07:16:27 -0300

    Script de alerta de vencimento finalizado

--- src/main/java/com/exemplo/JavaScript/alertVencimento.js
+++ src/main/java/com/exemplo/JavaScript/alertVencimento.js
@@ -1,6 +1,26 @@
-/* 
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Other/javascript.js to edit this template
- */
+function verificarProdutosProximosVencimento() {
+    const linhasProdutos = document.querySelectorAll('table tbody tr');
 
+    const limiteDias = 7;
+
+    linhasProdutos.forEach(function (linhaProduto) {
+        const dataVencimento = linhaProduto.querySelector('td:nth-child(5)').textContent;
+
+        const dataVencimentoDate = new Date(dataVencimento);
+
+        const dataAtual = new Date();
+
+        const diferencaTempo = dataVencimentoDate.getTime() - dataAtual.getTime();
+
+        const diasRestantes = Math.ceil(diferencaTempo / (1000 * 60 * 60 * 24));
+
+        if (diasRestantes <= limiteDias) {
+            const nomeProduto = linhaProduto.querySelector('td:nth-child(2)').textContent;
+            const codigoProduto = linhaProduto.querySelector('td:nth-child(3)').textContent;
+            alert(`Produto perto do vencimento!\nNome: ${nomeProduto}\nCódigo: ${codigoProduto}\nData de Vencimento: ${dataVencimento}`);
+        }
+    });
+}
+
+window.onload = verificarProdutosProximosVencimento;
 
