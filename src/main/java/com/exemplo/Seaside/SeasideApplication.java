15a224ad9811e406bade444f31e14304c1ff474f
Thiago dos Santos <supremoedits12066@gmail.com>
2023-11-11 07:08:21 -0300

    html home finalizado

---
+++ src/main/java/com/exemplo/Seaside/SeasideApplication.java
@@ -0,0 +1,13 @@
+package com.exemplo.Seaside;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeasideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeasideApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/exemplo/Seaside/SeasideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exemplo.Seaside;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeasideApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
